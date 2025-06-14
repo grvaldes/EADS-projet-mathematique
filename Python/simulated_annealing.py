b5970f4110f3c7acfd0f678ed2b22fc9ad6e2d9c
Sami13000 <sami.soghaier@etu.univ-amu.fr>
2025-06-14 22:49:19 +0200

    Update simulated_annealing.py
    Ajout des commentaires explicatifs sur les principales lignes

--- Python/simulated_annealing.py
+++ Python/simulated_annealing.py
@@ -3,7 +3,7 @@ import math
 import matplotlib.pyplot as plt
 
 # Longueur d’un chemin
-
+# Calcul la somme des distances entre chaque ville
 def longueur_chemin(chemin, matrice_distances):
     return sum(
         matrice_distances[chemin[i]][chemin[i + 1]]
@@ -15,36 +15,36 @@ def longueur_chemin(chemin, matrice_distances):
 
 def gen_voisin(chemin):
    
-    a, b = random.sample(range(1, len(chemin) - 2), 2)
+    a, b = random.sample(range(1, len(chemin) - 2), 2) # CHoisit deux positions aléatoires 
     voisin = chemin[:]
-    voisin[a], voisin[b] = voisin[b], voisin[a]
+    voisin[a], voisin[b] = voisin[b], voisin[a] # On échange les deux villes
     return voisin
 
 
 # Recuit simulé
 
-def recuit_simule(matrice_distances, chemin_initial, temperature_initiale=5000, alpha=0.999, iterations_max=20000):
-    courant = chemin_initial[:]
-    meilleur = courant[:]
+def recuit_simule(matrice_distances, chemin_initial, temperature_initiale=5000, alpha=0.999, iterations_max=20000): # Chemin initial = heuristique de départ, temp. initiale = contrôle la proba. d'accepter des mauvaises solutions, alpha = diminue la temp.
+    courant = chemin_initial[:] # On copie le chemin actuel
+    meilleur = courant[:] # Meilleure solution 
     distance_courante = longueur_chemin(courant, matrice_distances)
     meilleure_distance = distance_courante
     temperature = temperature_initiale
 
     for _ in range(iterations_max):
-        voisin = gen_voisin(courant)
+        voisin = gen_voisin(courant) # Génère une solution voisine
         distance_voisin = longueur_chemin(voisin, matrice_distances)
         delta = distance_voisin - distance_courante
 
-        if delta < 0 or random.random() < math.exp(-delta / temperature):
+        if delta < 0 or random.random() < math.exp(-delta / temperature): # Si la solution est meilleure, on l'accepte sinon, on peut l'accepter selon la température
             courant = voisin
             distance_courante = distance_voisin
             if distance_voisin < meilleure_distance:
                 meilleur = voisin
                 meilleure_distance = distance_voisin
 
-        temperature *= alpha
+        temperature *= alpha # On réduit la température
         if temperature < 1e-3:
-            break
+            break # On arrête si la température est trop basse
 
     return meilleur, meilleure_distance
 
