bbd342efe3e506694b1e3c1e05a2f9c3551fe015
Sami13000 <sami.soghaier@etu.univ-amu.fr>
2025-06-15 12:14:15 +0200

    Update insertion_heuristic.py
    Correction et ajout de quelques lignes

--- Python/insertion_heuristic.py
+++ Python/insertion_heuristic.py
@@ -1,5 +1,6 @@
 import random
 import math
+from time import perf_counter
 import matplotlib.pyplot as plt
 
 # Générer villes
@@ -31,9 +32,9 @@ def heuristique_insert(villes, matrice_distances):
     n = len(villes)
     non_visitees = list(range(n))
     
-    chemin = non_visitees[:3] # On commence par 3 villes
+    chemin_init = non_visitees[:3] # On commence par 3 villes
     non_visitees = non_visitees[3:]
-    chemin.append(chemin[0])  # fermer le cycle (retour à celle de départ)
+    chemin_init.append(chemin[0])  # fermer le cycle (retour à celle de départ)
 # tant qu'il reste des villes non visitées
     while non_visitees:
         meilleur_cout = float('inf') # coût d'insert minimal
@@ -41,9 +42,9 @@ def heuristique_insert(villes, matrice_distances):
         meilleure_position = None
 # Ici on recherche la meilleure ville à insérer entre deux villes
         for ville in non_visitees:
-            for i in range(1, len(chemin)):
-                a = chemin[i - 1]
-                b = chemin[i]
+            for i in range(1, len(chemin_init)):
+                a = chemin_init[i - 1]
+                b = chemin_init[i]
                 cout = (
                     matrice_distances[a][ville] +
                     matrice_distances[ville][b] -
@@ -54,14 +55,14 @@ def heuristique_insert(villes, matrice_distances):
                     meilleure_ville = ville
                     meilleure_position = i
 # On l'insère au meilleur endroit
-        chemin.insert(meilleure_position, meilleure_ville)
+        chemin_init.insert(meilleure_position, meilleure_ville)
         non_visitees.remove(meilleure_ville)
 # On calcule la distance totale
     distance_totale = 0.0
-    for i in range(len(chemin) - 1):
-        distance_totale += matrice_distances[chemin[i]][chemin[i + 1]]
+    for i in range(len(chemin_init) - 1):
+        distance_totale += matrice_distances[chemin_init[i]][chemin_init[i + 1]]
 
-    return chemin, distance_totale
+    return chemin_init, distance_totale
 
 
 # Affichage du chemin
@@ -82,7 +83,9 @@ def afficher_chemin(villes, chemin):
 
 villes = gen_villes(20) # On généère 20 villes aléatoires
 matrice = constr_matrice_distances(villes) # On construit la matrice des distances
-chemin, distance_totale = heuristique_insert(villes, matrice) # On calcul le chemin avec heuristique
-
+start_insert = perf_counter()
+chemin_init, distance_totale = heuristique_insert(villes, matrice) # On calcul le chemin avec heuristique
+end_insert = perf_counter()
+temps_insert = end_insert - start_insert
 print("Distance totale :", round(distance_totale, 2))
-afficher_chemin(villes, chemin) # Ona ffiche le graphe du chemin
+afficher_chemin(villes, chemin_init) # On affiche le graphe du chemin
